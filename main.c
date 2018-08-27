1d9d86418a8e77a0270b5cff4ba97c9c4106b750
Brian Kernighan <fakeuser@fake.com>
2018-08-27 08:58:33 -0400

    forgot to update version string

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20180823";
+const char	*version = "version 20180827";
 
 #define DEBUG
 #include <stdio.h>
