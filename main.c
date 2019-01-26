f25e845cf7a09d7b43c7a07e535a2e8d7f63aee1
Arnold D. Robbins <arnold@skeeve.com>
2019-01-26 21:55:03 +0200

    Update version string in main.c.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20180827";
+const char	*version = "version 20190125";
 
 #define DEBUG
 #include <stdio.h>
