416c6db5ee829b5e97dc8001b28e475fd942dbb6
Arnold D. Robbins <arnold@skeeve.com>
2019-12-08 21:43:32 +0200

    Update version and FIXES.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20191110";
+const char	*version = "version 20191208";
 
 #define DEBUG
 #include <stdio.h>
