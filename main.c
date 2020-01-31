78c79c06d07735d5881ea08cf40e014a41d341af
Arnold D. Robbins <arnold@skeeve.com>
2020-01-31 08:40:11 +0200

    Fix a{0}, update tests.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20200124";
+const char	*version = "version 20200131";
 
 #define DEBUG
 #include <stdio.h>
