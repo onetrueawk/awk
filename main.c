5068d20ef6ec53686488f1e48bd99e35e98efa40
Arnold D. Robbins <arnold@skeeve.com>
2020-02-06 22:27:31 +0200

    Restore zoulas fixes, step 1.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20200131";
+const char	*version = "version 20200206";
 
 #define DEBUG
 #include <stdio.h>
