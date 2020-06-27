onetrueawk/awk

commit 1d6ddfd9c0ed7119c0601474cded3a385068e886

Optimize string concatenation.

run.c
@@ -1196,13 +1196,14 @@ Cell *cat(Node **a, int q)	/* a[0] cat a[1] */
 
 	x = execute(a[0]);
 	n1 = strlen(getsval(x));
-	adjbuf(&s, &ssz, n1 + 1, recsize, 0, "cat1");
-	(void) strncpy(s, x->sval, ssz);
 
 	y = execute(a[1]);
 	n2 = strlen(getsval(y));
-	adjbuf(&s, &ssz, n1 + n2 + 1, recsize, 0, "cat2");
-	(void) strncpy(s + n1, y->sval, ssz - n1);
+
+	adjbuf(&s, &ssz, n1 + n2 + 1, recsize, 0, "cat");
+	memcpy(s, x->sval, n1);
+	memcpy(s + n1, y->sval, n2);
+	s[n1 + n2] = '\0';
 
 	tempfree(x);
 	tempfree(y);

FIXES
@@ -28,6 +28,8 @@ was sent to the printers in August, 1987.
 October 24, 2019:
 	Import second round of code cleanups from NetBSD. Much thanks
 	to Christos Zoulas (Github user zoulasc). Merges PR 53.
+	Add an optimization for string concatenation, also from
+	Christos.
 
 October 17, 2019:
 	Import code cleanups from NetBSD. Much thanks to Christos

commit 1119ed97e4444455e30659d586b166c1ed9615dd

Fix concatenation regression
The optimization in commit 1d6ddfd9c0ed7119c0601474cded3a385068e886
reintroduced the regression that was fixed in commit
e26237434fb769d9c1ea239101eb5b24be588eea.

run.c
@@ -1192,12 +1192,12 @@ Cell *cat(Node **a, int q)	/* a[0] cat a[1] */
 
 	x = execute(a[0]);
 	n1 = strlen(getsval(x));
+	adjbuf(&s, &ssz, n1, recsize, 0, "cat1");
+	memcpy(s, x->sval, n1);
 
 	y = execute(a[1]);
 	n2 = strlen(getsval(y));
-
-	adjbuf(&s, &ssz, n1 + n2 + 1, recsize, 0, "cat");
-	memcpy(s, x->sval, n1);
+	adjbuf(&s, &ssz, n1 + n2 + 1, recsize, 0, "cat2");
 	memcpy(s + n1, y->sval, n2);
 	s[n1 + n2] = '\0';
 