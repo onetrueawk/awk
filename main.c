c0f4e97e4561ff42544e92512bbaf3d7d1f6a671
Arnold D. Robbins <arnold@skeeve.com>
2021-02-15 20:33:15 +0200

    Fix compiling with g++.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20210106";
+const char	*version = "version 20210215";
 
 #define DEBUG
 #include <stdio.h>
