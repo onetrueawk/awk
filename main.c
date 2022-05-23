f009bd339788ee01539a74b68a9daf59969163bf
Miguel Pineiro Jr <mpj@pineiro.cc>
2022-05-23 17:14:42 -0400

    Fix memory leak in string assignment to built-ins
    The new string value is always freeable (strdup'd by tostring), so
    never mark it as DONTFREE.
    
    $ paste <(./leak.sh master.out) <(./leak.sh fixed.out)
     2564	 2464
     7260	 2476
    11948	 2476
    16636	 2476
    21324	 2476
    26012	 2476
    30700	 2476
    35388	 2476
    40076	 2476
    44760	 2476
    49448	 2476
    
    $ cat leak.sh
    #/bin/sh
    
    awk=${1:-nawk}
    
    # Print the initial resident set size
    # Assign each record to FS
    # Print rsz every 100,000 records
    # Exit after a million
    
    yes abcdefghijklmnopqrstuvwxyz | $awk -v mem='ps -p $PPID -o rsz=' '
        BEGIN            { system(mem) }
                         { FS = $0 }
        NR % 100000 == 0 { system(mem) }
        NR == 1000000    { exit }
    '

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20220303";
+const char	*version = "version 20220523";
 
 #define DEBUG
 #include <stdio.h>
