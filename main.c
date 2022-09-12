9e248c317b88470fc86aa7c988919dc49452c88c
ozan yigit <ozan.yigit@gmail.com>
2022-09-12 10:51:16 -0400

    updated version

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20220830";
+const char	*version = "version 20220912";
 
 #define DEBUG
 #include <stdio.h>
