5b9d0b92a9734854eea52ec1ead5355c62797bdd
ozan yigit <ozan.yigit@gmail.com>
2023-09-11 10:17:08 -0400

    updated version string

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20230909";
+const char	*version = "version 20230911";
 
 #define DEBUG
 #include <stdio.h>
