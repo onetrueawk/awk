9e254e503f844e122870e9488db3d7b0233e554c
ozan yigit <ozan.yigit@gmail.com>
2023-11-16 14:56:49 -0500

    adjusted version date.

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20231030";
+const char	*version = "version 20231116";
 
 #define DEBUG
 #include <stdio.h>
