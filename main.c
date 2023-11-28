dbf7cbed8645e65df5bba0c83b566e2bd96e63b6
ozan yigit <ozan.yigit@gmail.com>
2023-11-27 23:27:44 -0500

    adjust version date: 20231127

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20231124";
+const char	*version = "version 20231127";
 
 #define DEBUG
 #include <stdio.h>
