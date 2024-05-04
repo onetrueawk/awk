f8be48fc79d6508189794d751e1021b91452f660
ozan yigit <ozan.yigit@gmail.com>
2024-05-04 14:10:29 -0400

    adjust version date: 20240504

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20240503";
+const char	*version = "version 20240504";
 
 #define DEBUG
 #include <stdio.h>
